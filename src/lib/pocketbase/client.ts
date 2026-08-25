3f68eee65fd1f64db04d9ef09833a41a666ff696
samuel-txs <261601373+samuel-txs@users.noreply.github.com>
2026-08-25 11:46:32 -0300

    fix(pocketbase): set correct fallback backend url in client to prevent 405 on auth

--- src/lib/pocketbase/client.ts
+++ src/lib/pocketbase/client.ts
@@ -1,6 +1,9 @@
 import PocketBase from 'pocketbase'
 
-const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL)
+const pbUrl =
+  import.meta.env.VITE_POCKETBASE_URL ||
+  'https://landing-page-cartorios-cnj-0e353.shrd00.internal.goskip.dev'
+const pb = new PocketBase(pbUrl)
 pb.autoCancellation(false)
 
 export default pb
